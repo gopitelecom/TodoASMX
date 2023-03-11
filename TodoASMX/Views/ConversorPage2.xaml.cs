56f5958db7d25d4cd012fb7a01b48b3042638d2c
jjdh777 <76835447+jjdh777@users.noreply.github.com>
2023-03-11 10:45:04 +0000

    page2

---
+++ TodoASMX/Views/ConversorPage2.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TodoASMX.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ConversorPage2 : ContentPage
+    {
+        public ConversorPage2()
+        {
+            InitializeComponent();
+        }
+    }
+}
